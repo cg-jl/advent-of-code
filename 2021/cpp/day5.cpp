418e1316abffe48973cec9dc8a5746ead9c19775
CyberGsus <cyber.gsuscode@gmail.com>
2021-12-06 15:53:18 +0100

    day5 + reading utility (cpp)

---
+++ 2021/cpp/day5.cpp
@@ -0,0 +1,98 @@
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "util.h"
+
+struct Point {
+    int x, y;
+};
+
+struct Line {
+    Point start, end;
+
+    constexpr auto is_vertical() const -> bool { return start.x == end.x; }
+    constexpr auto is_horizontal() const -> bool { return start.y == end.y; }
+};
+
+auto operator<<(std::ostream& stream, const Point& p) -> std::ostream& {
+    return stream << p.x << ',' << p.y;
+}
+
+auto operator<<(std::ostream& stream, const Line& line) -> std::ostream& {
+    return stream << line.start << " -> " << line.end;
+}
+
+auto parse_point(std::string_view source) -> Point {
+    auto found = source.find(',');
+    return {std::atoi(source.substr(0, found).data()),
+            std::atoi(source.substr(found + 1).data())};
+}
+
+auto parse_line(std::string_view source) -> Line {
+    auto found = source.find("->");
+    return {parse_point(source.substr(0, found)),
+            parse_point(source.substr(found + 3))};
+}
+
+auto parse_input(std::string_view filename) -> util::InputReader<Line> {
+    return util::InputReader<Line>(filename, parse_line);
+}
+
+#define MAT_W 1000
+
+auto plot_line(int* matrix, const Line& line) -> void {
+    if (line.is_vertical()) {
+        const std::size_t col = line.start.x;
+        const std::size_t start = std::min(line.start.y, line.end.y);
+        const std::size_t end = std::max(line.start.y, line.end.y);
+        for (std::size_t row = start; row <= end; ++row) {
+            matrix[row * MAT_W + col]++;
+        }
+    } else if (line.is_horizontal()) {
+        const std::size_t row = line.start.y;
+        const std::size_t start = std::min(line.start.x, line.end.x);
+        const std::size_t end = std::max(line.start.x, line.end.x);
+        for (std::size_t col = start; col <= end; ++col) {
+            matrix[row * MAT_W + col]++;
+        }
+    } else {
+        // let's see how the slope is:
+        // slope is always going to be 1 or -1 (45 degree)
+        const int m = (line.end.y - line.start.y) / (line.end.x - line.start.x);
+        // now the N value:
+        const int n = line.end.y - line.end.x * m;
+
+
+        const std::size_t col_start = std::min(line.end.x, line.start.x);
+        const std::size_t col_end = std::max(line.end.x, line.start.x);
+
+        for (std::size_t col = col_start; col <= col_end; ++col) {
+            auto row = int(col) * m + n;
+            matrix[row * MAT_W + col]++;
+        }
+    }
+}
+
+auto main() -> int {
+    auto matrix = std::make_unique<int[]>(MAT_W * MAT_W);
+    std::memset(matrix.get(), 0, MAT_W * MAT_W * sizeof(int));
+    auto reader = parse_input("inputs/day5.txt");
+
+    for (const auto& line : reader) {
+        plot_line(matrix.get(), line);
+    }
+
+
+    std::size_t count = 0;
+    for (std::size_t i = 0; i < MAT_W * MAT_W; ++i) {
+        count += matrix[i] >= 2;
+    }
+    std::cout << count << '\n';
+
+    return 0;
+}
---
+++ 2021/cpp/util.h
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include <functional>
+#include <optional>
+#include <string_view>
+
+namespace util {
+template <typename T>
+class InputReader {
+    std::ifstream input;
+    std::function<T(std::string_view)> m_line_parser;
+    std::string linebuf;
+
+   public:
+    class iterator {
+        InputReader<T>& m_reader;
+        bool m_is_end = false;
+        std::string m_line = "";
+
+       public:
+        iterator(InputReader<T>& reader, std::string_view line, bool is_end = false)
+            : m_reader(reader), m_is_end(is_end), m_line(line) {}
+        iterator& operator++() {
+            if (m_is_end) return *this;  // do not try to advance
+            const auto maybe_next_line = m_reader.current_line();
+            if (!maybe_next_line)
+                m_is_end = true;
+            else {
+                m_line = *maybe_next_line;
+            }
+            return *this;
+        }
+        /// Parses the current line with the given function
+        T operator*() { return m_reader.parse(m_line); }
+        bool operator==(const iterator& other) {
+            // this operator is only going to be used in order to check
+            return m_is_end ? other.m_is_end : false;
+        }
+        bool operator!=(const iterator& other) { return !(*this == other);}
+    };
+
+    InputReader(std::string_view filename,
+                std::function<T(std::string_view)> line_parser)
+        : input(filename.data()), m_line_parser(line_parser) {}
+
+    InputReader(std::ifstream&& stream,
+                std::function<T(std::string_view)> line_parser)
+        : m_input(stream), m_line_parser(line_parser) {}
+    T parse(std::string_view line) { return m_line_parser(line); }
+    std::optional<std::string_view> current_line() {
+        if (std::getline(input, linebuf)) {
+            return {std::string_view(linebuf)};
+        } else {
+            return {};
+        }
+    }
+
+    iterator begin() { 
+        const auto maybe_line = current_line();
+        if (!maybe_line) {
+            return end(); // already consumed
+        } else {
+            return iterator(*this, *maybe_line);
+        }
+     }
+    iterator end() { return iterator(*this, "", true); }
+};
+}  // namespace util
