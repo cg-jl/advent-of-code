86df9e096a450c2a8067cc5abda7bd99e1328521
CyberGsus <cyber.gsuscode@gmail.com>
2021-12-07 13:53:12 +0100

    Format day6 (cpp)

--- 2021/cpp/day6.cpp
+++ 2021/cpp/day6.cpp
@@ -1,18 +1,19 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <numeric>
-#include <string_view>
-#include <string>
 #include <cstring>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <string_view>
+#include <vector>
 
 typedef unsigned char byte;
 
 auto read_input(std::string_view line) -> std::vector<std::size_t> {
     std::vector<std::size_t> amt(9);
     std::memset(amt.data(), 0, amt.size());
-    for (std::size_t found = line.find(','); found != line.npos; found = line.find(',')) {
-        amt[std::atoi(line.substr(0,  found).data())]++;
+    for (std::size_t found = line.find(','); found != line.npos;
+         found = line.find(',')) {
+        amt[std::atoi(line.substr(0, found).data())]++;
         line = line.substr(found + 1);
     }
     if (!line.empty()) {
@@ -30,25 +31,20 @@ auto print_world(const std::vector<std::size_t>& world) -> void {
 }
 
 auto day(std::vector<std::size_t>& world) -> void {
-    const auto current_size = world.size();
     const auto timeouted = world[0];
 
     // process the ones that are already there
     for (std::size_t i = 1; i <= 8; i++) {
-        world[i - 1] = world[i];       
+        world[i - 1] = world[i];
     }
 
     // add the next generation
-    world[8]  = timeouted;
+    world[8] = timeouted;
     // reset the other timers
     world[6] += timeouted;
-
-
-    
 }
 
-
-auto  main() -> int {
+auto main() -> int {
     std::string line;
     {
         std::ifstream input("inputs/day6.txt");
@@ -61,6 +57,8 @@ auto  main() -> int {
     for (int i = 0; i < 256; ++i) {
         day(world);
     }
-    std::cout << std::accumulate<decltype(world)::const_iterator, std::size_t>(world.cbegin(), world.cend(), 0) << '\n';
+    std::cout << std::accumulate<decltype(world)::const_iterator, std::size_t>(
+                     world.cbegin(), world.cend(), 0)
+              << '\n';
     return 0;
 }
